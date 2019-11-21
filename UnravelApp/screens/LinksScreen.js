2bd38d91879a0b8c6ad33fba3f7e11eb341601c7
Jenn June <jennjuneux@gmail.com>
2019-11-20 17:54:38 -0800

    created component TopNav

--- UnravelApp/screens/LinksScreen.js
+++ UnravelApp/screens/LinksScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-
+import HeaderIconTextButtonExample, {TopNav} from '../components/TopNav';
 export default function LinksScreen() {
+
   return (
-    <ScrollView style={styles.container}>
+    <HeaderIconTextButtonExample style={styles.container}>
       {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
       <ExpoLinksView />
-    </ScrollView>
+    </HeaderIconTextButtonExample>
+
   );
 }
 
