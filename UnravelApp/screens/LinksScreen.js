f79a15aa7cffa9971b351aaa51dc3b9e9c4a4e05
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2019-11-20 17:36:45 -0800

    creates input form component and tests on Link screen.js

--- UnravelApp/screens/LinksScreen.js
+++ UnravelApp/screens/LinksScreen.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import TextArea, { InputForm } from '../components/InputForm';
 
 export default function LinksScreen() {
   return (
-    <ScrollView style={styles.container}>
+    <TextArea style={styles.container}>
       {/**
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
       <ExpoLinksView />
-    </ScrollView>
+    </TextArea>
   );
 }
 
