a6aceac3653cd76af4dcfda68921df297f749fea
Jenn June <jennjuneux@gmail.com>
2019-11-30 14:30:21 -0800

    added image to Learn page

--- UnravelApp/screens/LearnScreen.js
+++ UnravelApp/screens/LearnScreen.js
@@ -1,43 +1,42 @@
-import React, { Component } from "react";
-import { Container, Header, H1, Content, Text, Accordion } from "native-base";
-const dataArray = [
-    { 
-        title: "What is Unravel really about?", 
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-    { 
-        title: "Your thoughts are fascinating!", 
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-    { 
-        title: "Emotions vs thoughts: where we get stuck!", 
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-    { 
-        title: "Creating space: finding your balance.", 
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
-        
-
-];
+import React from 'react';
+import {Image} from 'react-native';
+import { Container, Content, Body, Text, H1, H2 } from 'native-base';
+// import {Media, Jumbotron} from 'react-bootstrap/Media'
+//import QuestionHeader from '../components/Questions/Question';
 
 export default function LearnScreen() {
-
+    /**
+     * Go ahead and delete ExpoConfigView and replace it with your content;
+     * we just wanted to give you a quick view of your config.
+     */
     return (
         <Container>
-            {/* <Header /> */}
-            <Content padder>
-                <Text>
-                    <H1>Let's Unravel!</H1>
+            <Content>
+                <Image source={require('../assets/images/UnravelSun.png')}/>
+                <Text> 
+                    <H1>Unravelling Thoughts</H1>
+                    
+                    <H2>Journey to the Curiosity of Self</H2> 
+                    <H2>What is Unravel really about?</H2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               
+                    <H2>Investigate your thoughts. They are fascinating!</H2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
+                    <H2>Understanding emotions versus thoughts--where we get stuck!</H2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
+                    <H2>This is about finding your balance and creating a space of calm.</H2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                
+                <H2>Keep focused. Stay engaged. YOU GOT THIS!</H2>
+
                 </Text>
-                <Accordion
-                    dataArray={dataArray}
-                    headerStyle={{ backgroundColor: "#b2ebf2" }}
-                    contentStyle={{ backgroundColor: "#f4f4f4" }}
-                />
             </Content>
         </Container>
+
     );
 };
-
-
-
 
 LearnScreen.navigationOptions = {
     title: 'Learn',
