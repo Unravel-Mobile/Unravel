5936d0cc71bca050143b9e44ac2ce85db3e1a8f9
Blue Bean <sl8305@hotmail.com>
2020-01-25 13:43:02 -0800

    commented Home.js page

--- UnravelApp/components/Home.js
+++ UnravelApp/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, Dimensions, } from 'react-native';
-import { Container, Content, Text, Button, View } from 'native-base';
+import { Container, Text, Button, View } from 'native-base';
 import Styles from './Style/Style';
 
 var { width } = Dimensions.get("window");
@@ -10,29 +10,39 @@ export default HomePage = (props) => {
 
     return (
         <Container style={Styles.mainBody}>
-            {/* <Content> */}
+
+                {/* Opening Logo for Unravel  */}
                 <View style={Styles.logo}>
                     <Image source={require('../assets/images/unravel01.png')} style={Styles.logoImg} />
+                    
+                    {/* Title */}
                     <Text style={Styles.headerOne}>
                         UNRAVEL
-                </Text>
+                    </Text>
+
+                    {/* Caption for title */}
                     <Text style={Styles.headerTwo}>
                         Break down thoughts. Find clarity.
-                </Text>
+                    </Text>
+
+                    {/* Button for creating thought log */}
+                    {/* - navigates to first log page */}
                     <Button block bordered dark onPress={() => props.navigate(props.log)}>
                         <Text style={[Styles.text]}>Unravel a thought</Text>
                     </Button>
 
+                    {/* Button for reviewing logs */}
+                    {/* - navigates to review page */}
                     <Button block bordered dark onPress={() => props.navigate(props.review)}>
                         <Text style={[Styles.text]}>Review thought entries</Text>
                     </Button>
                     
+                    {/* Button for learning more about thought logs */}
+                    {/* - navigates to learn page */}
                     <Button block bordered dark onPress={() => props.navigate(props.learn)}>
                         <Text style={[Styles.text]}>Learn about Unravel</Text>
                     </Button>
                 </View>
-
-            {/* </Content> */}
 
         </Container>
     );
