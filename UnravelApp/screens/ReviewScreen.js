a240b92c87d07d443e10321fd48d461ed9e57510
Blue Bean <sl8305@hotmail.com>
2020-01-31 14:39:44 -0800

    learn screen shows all information except the image and padding. pushing to sync with marino first

--- UnravelApp/screens/ReviewScreen.js
+++ UnravelApp/screens/ReviewScreen.js
@@ -13,18 +13,9 @@ export default function ReviewScreen() {
   return (
     <Container>
       <Content padder>
-<<<<<<< HEAD
-
-        <Text style={Styles.headNav}>Thoughts saved</Text>
-  
-        <GetCall />
-
-      </Content>
-=======
             <Text style={Styles.headNav}>Thoughts saved</Text>
         <GetCall />
         </Content>
->>>>>>> 21f31429af9fb56d5fcbfca36b93d27dc3a05ca1
     </Container>
 
   );
