587ad8dadbbb32ad988765cb42266ee0e560a01e
sl8305 <50194275+sl8305@users.noreply.github.com>
2020-02-02 17:46:28 -0800

    Update GetCall.js
    Adjusting thumbnail on line 35 of the get call as suggested by marino.
    The source was not referencing correctly after the file restructure. This correction should fix it.

--- UnravelApp/components/GetCall/GetCall.js
+++ UnravelApp/components/GetCall/GetCall.js
@@ -32,7 +32,7 @@ export default class GetCall extends Component {
                                 {/* <Thumbnail square source={{ uri: 'Image URL' }} /> */}
                             </ListItem>
                             <Button style={Styles.reviewButton} transparent onPress={() => Alert.alert(`${thought.wordSelect1}`)}>
-                                <Thumbnail square source={require('../assets/images/unravel01.png')} />
+                                <Thumbnail square source={require('../../assets/images/unravel01.png')} />
                                 <Text style={Styles.reviewSubHead}>{thought.title}</Text>
                             </Button>
                             <ListItem style={Styles.date}>
