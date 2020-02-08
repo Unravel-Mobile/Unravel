4ac1a5c56d073204fc261d1fb4b4c3318b203956
Blue Bean <sl8305@hotmail.com>
2020-02-08 15:31:05 -0800

    going to push into development

--- UnravelApp/screens/HomeScreen.js
+++ UnravelApp/screens/HomeScreen.js
@@ -34,11 +34,12 @@ export default class HomeScreen extends Component {
     console.log('Logged in!');
   };
 
+  // method to set new user and save to async storage
+  // async storage schema: {key => deviceId, value => dbId}
   _storeData = async (_id, userId) => {
     try {
       await AsyncStorage.setItem(userId, _id);
       console.log('_store data - - > ', userId, ' : ', _id);
-
     } catch (error) {
       console.log(error);
     }
@@ -66,7 +67,7 @@ export default class HomeScreen extends Component {
   // };
 
   // method to check if user exists in async storage
-  // async storage schema: {key => deviceId, value => fireBaseId}
+  // async storage schema: {key => deviceId, value => dbId}
   _userExistsInDataBase = async(uid) => {
     try {
       var user = await AsyncStorage.getItem(uid);
@@ -90,8 +91,8 @@ export default class HomeScreen extends Component {
       if (user) {
         // User is signed in.
         var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        console.log('homescreen line 94 uid - > ', uid);
+        var uid = user.uid; // device id
+        console.log('homescreen line 95 uid - > ', uid);
 
         this._userExistsInDataBase(uid).then((userExistsInDataBase) => {
           if(!userExistsInDataBase) {
@@ -109,9 +110,7 @@ export default class HomeScreen extends Component {
       } else {
         // User is not signed in.
         console.log("User is not signed in.");
-        // ...
       }
-      // ...
     });
   }
 
